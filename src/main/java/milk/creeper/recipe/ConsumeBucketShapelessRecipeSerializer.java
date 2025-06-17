c0155f8fb298e9c02d033241d16cac055355cc75
Cardiffht001 <cardiffht001@gmail.com>
2025-06-16 19:38:54 -0500

    1.20.5 version

--- src/main/java/milk/creeper/recipe/ConsumeBucketShapelessRecipeSerializer.java
+++ src/main/java/milk/creeper/recipe/ConsumeBucketShapelessRecipeSerializer.java
@@ -5,12 +5,13 @@ import com.mojang.serialization.DataResult;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.ShapelessRecipe;
 import net.minecraft.recipe.book.CraftingRecipeCategory;
-import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.util.collection.DefaultedList;
 
 public class ConsumeBucketShapelessRecipeSerializer implements RecipeSerializer<ConsumeBucketShapelessRecipe> {
@@ -20,7 +21,7 @@ public class ConsumeBucketShapelessRecipeSerializer implements RecipeSerializer<
     private static final MapCodec<ConsumeBucketShapelessRecipe> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
             Codec.STRING.optionalFieldOf("group", "").forGetter(ShapelessRecipe::getGroup),
             CraftingRecipeCategory.CODEC.fieldOf("category").orElse(CraftingRecipeCategory.MISC).forGetter(ShapelessRecipe::getCategory),
-            ItemStack.RECIPE_RESULT_CODEC.fieldOf("result").forGetter(recipe -> recipe.getResult(null)),
+            ItemStack.CODEC.fieldOf("result").forGetter(recipe -> recipe.getResult(null)),
             Ingredient.DISALLOW_EMPTY_CODEC.listOf().fieldOf("ingredients").flatXmap(ingredients -> {
                 Ingredient[] ingredientsArray = ingredients.stream().filter(ingredient -> !ingredient.isEmpty()).toArray(Ingredient[]::new);
                 if (ingredientsArray.length == 0) {
@@ -33,32 +34,27 @@ public class ConsumeBucketShapelessRecipeSerializer implements RecipeSerializer<
             }, DataResult::success).forGetter(ShapelessRecipe::getIngredients)
     ).apply(instance, ConsumeBucketShapelessRecipe::new));
 
+    private static final PacketCodec<RegistryByteBuf, ConsumeBucketShapelessRecipe> PACKET_CODEC = PacketCodec.tuple(
+            PacketCodecs.STRING, ShapelessRecipe::getGroup,
+            CraftingRecipeCategory.PACKET_CODEC, ShapelessRecipe::getCategory,
+            ItemStack.PACKET_CODEC, recipe -> recipe.getResult(null),
+            Ingredient.PACKET_CODEC.collect(PacketCodecs.toList()).xmap(
+                ingredients -> {
+                    Ingredient[] ingredientsArray = ingredients.stream().filter(ingredient -> !ingredient.isEmpty()).toArray(Ingredient[]::new);
+                    return DefaultedList.copyOf(Ingredient.EMPTY, ingredientsArray);
+                },
+                defaultedList -> defaultedList.stream().toList()
+            ), ShapelessRecipe::getIngredients,
+            ConsumeBucketShapelessRecipe::new
+    );
+
     @Override
-    public Codec<ConsumeBucketShapelessRecipe> codec() {
-        return CODEC.codec();
+    public MapCodec<ConsumeBucketShapelessRecipe> codec() {
+        return CODEC;
     }
 
     @Override
-    public ConsumeBucketShapelessRecipe read(PacketByteBuf buf) {
-        String group = buf.readString();
-        CraftingRecipeCategory category = buf.readEnumConstant(CraftingRecipeCategory.class);
-        int i = buf.readVarInt();
-        DefaultedList<Ingredient> ingredients = DefaultedList.ofSize(i, Ingredient.EMPTY);
-        for (int j = 0; j < ingredients.size(); ++j) {
-            ingredients.set(j, Ingredient.fromPacket(buf));
-        }
-        ItemStack itemStack = buf.readItemStack();
-        return new ConsumeBucketShapelessRecipe(group, category, itemStack, ingredients);
-    }
-
-    @Override
-    public void write(PacketByteBuf buf, ConsumeBucketShapelessRecipe recipe) {
-        buf.writeString(recipe.getGroup());
-        buf.writeEnumConstant(recipe.getCategory());
-        buf.writeVarInt(recipe.getIngredients().size());
-        for (Ingredient ingredient : recipe.getIngredients()) {
-            ingredient.write(buf);
-        }
-        buf.writeItemStack(recipe.getResult(null));
+    public PacketCodec<RegistryByteBuf, ConsumeBucketShapelessRecipe> packetCodec() {
+        return PACKET_CODEC;
     }
 } 
